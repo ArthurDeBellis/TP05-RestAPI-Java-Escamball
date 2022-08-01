e40c90eff3143e790c6c35b55a9bdcf47b821e32
Arthur De Bellis <arthur.bellis@ufv.br>
2022-08-01 16:11:59 -0300

    Init Repository

---
+++ src/main/java/Escamball/Escamball/EscamballApplication.java
@@ -0,0 +1,13 @@
+package Escamball.Escamball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscamballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscamballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Escamball/Escamball/EscamballApplicationTests.java
@@ -0,0 +1,13 @@
+package Escamball.Escamball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscamballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
